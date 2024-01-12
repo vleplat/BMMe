a8c2718365a8a9da83642fb99223e8d1dcb7944a
nicolasgillis <nicolasgillis@gmail.com>
2024-01-12 11:02:45 +0100

    Update addPaths.m

--- addPaths.m
+++ addPaths.m
@@ -1,13 +1,4 @@
 clear all;close all;clc;
 
-%%-------------------------------------------------------------------------
-% Add paths to directories
-%--------------------------------------------------------------------------
-addpath("Datasets\");   % directory with data sets
-addpath("Methods\");    % directory with Proposed Algorithms
-addpath("Utils\");      % directory with utiliterian functions
-
-% below: libraries for benchmarked methods
-addpath("Libraries\group_robust_NMF\") ;
-addpath("Libraries\min_vol_KL_NMF\");
-addpath("Libraries\min_vol_KL_NMF\Utils_lib\");
+cd('./');
+addpath(genpath('./'));
